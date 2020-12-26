ecf2efab7817fb6308cf836ff2400bad1a1f4663
Leo Cappiello <leonelcappiello@gmail.com>
2020-12-26 15:56:05 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="palabra"> </i>',
+        '<i class="palabra">Neumáticos</i>',
+        '<i class="palabra">Llantas</i>',
+        '<i class="palabra">Accesorios</i>'
+    ],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 75,
+    backDelay: 500,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+});
+
